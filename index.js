7225c3a759515260f6fdd72774b21a9d9e1da2b0
Nattaphong2019 <47801213+Nattaphong2019@users.noreply.github.com>
2022-07-03 15:31:24 +0700

    feat: add funtion addClassCircleBlue

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-convertTimeToBinary=(timeString)=>{  
-    var binary = timeString.split('').map((time) => parseInt(time, 10).toString(2).padStart(4, '0'));
-    console.log(binary);
-}
+const convertTimeToBinary = (timeString) => {
+  let binary = timeString
+    .split("")
+    .map((time) => parseInt(time, 10).toString(2).padStart(4, "0"));
+  return binary;
+};
 
-getCurrentTime=()=>{
-    const date = new Date();
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
-    return `${hours}${minutes}${seconds}`;
-}
+const getCurrentTime = () => {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${hours}${minutes}${seconds}`;
+};
 
-convertTimeToBinary(getCurrentTime());
+const addClassCircleBlue = () => {
+  const binaryTimes = convertTimeToBinary(getCurrentTime());
+  for (let i = 0; i < 6; i++) {
+    const times = binaryTimes[i].split("");
+    for (let j = 0; j < 4; j++) {
+      const time = times[j].split("");
+      let element = document
+        .getElementsByClassName("col")
+        [i].getElementsByClassName("circle")[j];
+      if (time == 1) {
+        element.classList.add("circle-blue");
+      } else {
+        element.classList.remove("circle-blue");
+      }
+    }
+  }
+};
+
+setInterval(function () {
+  addClassCircleBlue();
+}, 1000);
